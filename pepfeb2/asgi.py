74cb0516b3fb01cfaf30ddefa9efe2357434d73d
Harsh Porwal <144673012+Harshporwal0904@users.noreply.github.com>
2026-02-04 22:47:53 +0530

    Add files via upload

---
+++ pepfeb2/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pepfeb2 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pepfeb2.settings')
+
+application = get_asgi_application()
